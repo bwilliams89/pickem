a5baccf9440dd44ee19e2912e46292e32e192fb6
Ben Williams <bwilliams89@gmail.com>
2012-12-23 15:31:33 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pickem::Application.config.secret_token = '9d1ac3ea37906bb19976a1b8b12764ad41599b59d2438c046e023852bc4af9b0bd38f26098108ca8e32860acd68a7d786033f4f6fd4e2faaa1467328e5a811dc'
